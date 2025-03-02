82ec70819df4d536308950219f3111346fa2ce05
Jaakko Airikkala <jaakkel@gmail.com>
2025-03-02 12:19:38 +0200

    Ex 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -7,20 +7,35 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const handleGood = () => {
     const newGood = good + 1;
     setGood(newGood);
+    const newTotal = total + 1;
+    setTotal(newTotal);
+    setAverage((newGood - bad) / newTotal);
+    setPositive(newGood / newTotal);
   };
 
   const handleNeutral = () => {
     const newNetutral = neutral + 1;
     setNeutral(newNetutral);
+    const newTotal = total + 1;
+    setTotal(newTotal);
+    setAverage((good - bad) / newTotal);
+    setPositive(good / newTotal);
   };
 
   const handleBad = () => {
     const newBad = bad + 1;
     setBad(newBad);
+    const newTotal = total + 1;
+    setTotal(newTotal);
+    setAverage((good - newBad) / newTotal);
+    setPositive(good / newTotal);
   };
 
   return (
@@ -38,6 +53,12 @@ const App = () => {
         neutral {neutral}
         <br />
         bad {bad}
+        <br />
+        all {total}
+        <br />
+        average {average}
+        <br />
+        positive {100 * positive} %
         <br />
       </div>
     </div>
