e9c7fc98bb868ecd7642790f8edba3ebc59836bc
Jaakko Airikkala <jaakkel@gmail.com>
2026-02-02 18:00:07 +0200

    Ex 3.1

---
+++ osa 3/puhelinluettelon-backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+  { name: "Arto Hellas", number: "040-123456" },
+  { name: "Ada Lovelace", number: "39-44-5323523" },
+  { name: "Dan Abramov", number: "12-43-234345" },
+  { name: "Mary Poppendieck", number: "39-23-6423122" },
+  { name: "Dr. Livingstone", number: "+260-12-03-1873" },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
