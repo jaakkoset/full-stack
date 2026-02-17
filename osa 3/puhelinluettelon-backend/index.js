46efa1bc24e67337ae5d73c21b1a0697eef0460b
Jaakko Airikkala <jaakkel@gmail.com>
2026-02-17 11:58:12 +0200

    3.13: puhelinluettelo ja tietokanta, step1

--- osa 3/puhelinluettelon-backend/index.js
+++ osa 3/puhelinluettelon-backend/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person");
 
 const app = express();
 
@@ -28,7 +30,9 @@ let persons = [
 ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -80,7 +84,7 @@ app.post("/api/persons", (request, response) => {
   response.json(persons);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
