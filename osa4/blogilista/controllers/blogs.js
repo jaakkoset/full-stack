e19361312cec37a195c131a16c5785834f631e8e
Jaakko Airikkala <jaakkel@gmail.com>
2026-06-26 17:54:21 +0300

    4.17: blogilistan laajennus, step5

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  })
   response.status(200).json(blogs)
 })
 
@@ -10,9 +15,19 @@ blogsRouter.post('/', async (request, response) => {
   if (!request.body.title || !request.body.url) {
     return response.status(400).end()
   }
-  const blog = new Blog(request.body)
-  const result = await blog.save()
-  response.status(201).json(result)
+  // Pick the first user
+  const users = await User.find({})
+  const user = users[0]
+  const userId = user.id
+  // Save the blog
+  const newBlog = request.body
+  newBlog['user'] = userId
+  const blog = new Blog(newBlog)
+  const savedBlog = await blog.save()
+  // Add the blog id in the users `blogs` field
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
