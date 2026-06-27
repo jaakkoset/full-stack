dc8cc0592607593fb82386d0b99cb71dbfc694a0
Jaakko Airikkala <jaakkel@gmail.com>
2026-06-27 20:30:27 +0300

    Add to error handling

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -44,7 +44,11 @@ const userExtractor = async (request, response, next) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
-  if (
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (
     error.name === 'MongoServerError' &&
     error.message.includes('E11000 duplicate key error')
   ) {
