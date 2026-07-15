6552c97f447d7b46523bd8e6ca8dcf19e1fb4aed
Jaakko Airikkala <jaakkel@gmail.com>
2026-07-15 16:15:11 +0300

    5.28: routed blogs, step5

--- osa5/end-to-end/tests/blog_app.spec.js
+++ osa5/end-to-end/tests/blog_app.spec.js
@@ -15,14 +15,9 @@ describe('Blog app', () => {
     await page.goto('http://localhost:5173')
   })
 
-  test('Login form is shown', async ({ page }) => {
-    const usernameTextbox = page.getByLabel('username')
-    const passwordTextbox = page.getByLabel('password')
-    const loginButton = page.getByRole('button', { name: 'login' })
-
-    await expect(usernameTextbox).toBeVisible()
-    await expect(passwordTextbox).toBeVisible()
-    await expect(loginButton).toBeVisible()
+  test('Login link is shown', async ({ page }) => {
+    const loginLink = page.getByText('Login')
+    await expect(loginLink).toBeVisible()
   })
 
   describe('Login', () => {
@@ -47,46 +42,55 @@ describe('Blog app', () => {
     })
 
     test('a new blog can be created', async ({ page }) => {
+      const title = 'A blog added by playwright'
+      const author = 'Author-name'
       await createBlog(page, 'A blog added by playwright', 'Author-name', 'url')
 
       await expect(
-        page.getByText('A blog added by playwright Author-name'),
+        page.getByText(`${title} by ${author}`),
       ).toBeVisible()
     })
 
+    test('Logout button is visible', async ({ page }) => {
+      const button = page.getByRole('button', { name: 'Logout' })
+
+      await expect(button).toBeVisible()
+    })
+
     describe('and a blog has been added', () => {
+      const blogTitle = 'A blog added by playwright'
+      const blogAuthor = 'Author-name'
       beforeEach(async ({ page }) => {
         await createBlog(
           page,
-          'A blog added by playwright',
-          'Author-name',
+          blogTitle,
+          blogAuthor,
           'url',
         )
       })
 
       test('user can like the blog', async ({ page }) => {
-        await page.getByRole('button', { name: 'View' }).click()
+        await page.getByText(`${blogTitle} by ${blogAuthor}`).click()
         await expect(page.getByText('Likes 0')).toBeVisible()
         await page.getByRole('button', { name: 'Like' }).click()
         await expect(page.getByText('Likes 1')).toBeVisible()
       })
 
       test('user can delete the blog', async ({ page }) => {
-        const addedBlog = 'A blog added by playwright Author-name'
-        await expect(page.getByText(addedBlog)).toBeVisible()
+        const visibleText = `${blogTitle} by ${blogAuthor}`
+        await page.getByText(visibleText).click()
 
-        await await page.getByRole('button', { name: 'View' }).click()
         // Accept window.confirm
         page.on('dialog', dialog => dialog.accept())
         await page.getByRole('button', { name: 'Remove' }).click()
 
         const message = page.getByText(
-          'Blog "A blog added by playwright" by Author-name removed',
+          `Blog "${blogTitle}" by ${blogAuthor} removed`,
         )
         // Notification message is visible
         await expect(message).toBeVisible()
         // No blog found anymore
-        await expect(page.getByText(addedBlog)).toHaveCount(0)
+        await expect(page.getByText(visibleText)).toHaveCount(0)
       })
 
       test('another user cannot see the remove button of the blog', async ({
@@ -101,20 +105,14 @@ describe('Blog app', () => {
             password: 'salainen',
           },
         })
-        // The user that added the blog can see the "Remove" button
-        await page.getByRole('button', { name: 'View' }).click()
-        await expect(page.getByRole('button', { name: 'Remove' })).toHaveCount(
-          1,
-        )
+        const visibleText = `${blogTitle} by ${blogAuthor}`
+
         // Login as another user
         await page.getByRole('button', { name: 'Logout' }).click()
         await loginWith(page, 'pepe', 'salainen')
-        // Blog is still visible
-        await expect(
-          page.getByText('A blog added by playwright Author-name'),
-        ).toBeVisible()
+        // Navigate to the blog
+        await page.getByText(visibleText).click()
         // User cannot see the "Remove" button
-        await page.getByRole('button', { name: 'View' }).click()
         await expect(page.getByRole('button', { name: 'Remove' })).toHaveCount(
           0,
         )
